946d59ae155779466adb3061b2c5be46adc34968
juanique <juanique@clopez-VirtualBox.(none)>
2012-03-29 12:33:48 -0400

    better interface

---
+++ bitmask/__init__.py
@@ -0,0 +1,52 @@
+class BitMaskOptions(object):
+
+    def __init__(self, options):
+
+        self._current = -1
+        self.options = {}
+        self.keys = options
+
+        for i in range(len(options)):
+            option = options[i]
+            self.options[option] = 1 << i
+            setattr(self, option, option)
+
+    def get_int(self, opt):
+        return self.options[opt]
+
+    def __iter__(self):
+        return self
+
+    def next(self):
+        if self._current >= len(self.keys) - 1:
+            self.current = 0
+            raise StopIteration
+        else:
+            self._current += 1
+            return self.keys[self._current]
+
+class BitMask(object):
+
+    def __init__(self, options, value=0):
+        self.value = value
+        self.options = options
+
+    def has(self, opt):
+        if type(opt) == str:
+            opt = self.options.options[opt]
+        return self.value & opt > 0
+
+    def add(self, opt):
+        if type(opt) == str:
+            opt = self.options.options[opt]
+        self.value = self.value | opt
+
+    def set(self, opt, enabled):
+        if type(opt) == str:
+            opt = self.options.options[opt]
+
+        if enabled:
+            self.value = self.value | opt
+        else:
+            if self.has(opt):
+                self.value = self.value ^ opt
---
+++ test.py
@@ -0,0 +1,90 @@
+import unittest
+from bitmask import BitMask, BitMaskOptions
+
+class BitMaskUnitTest(unittest.TestCase):
+
+    def test_init(self):
+        "It can be initialized given a set of options."
+
+        options = ['a', 'b', 'c']
+        BitMaskOptions(options)
+
+    def test_init_mask_with_options(self):
+        "The mask can be initialized with a set of options"
+
+        options = BitMaskOptions(['a', 'b', 'c'])
+        BitMask(options=options, value=15)
+
+class BitMaskManipulateValues(unittest.TestCase):
+
+    def setUp(self):
+        self.options = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun']
+        self.weekdays = BitMaskOptions(self.options)
+
+    def test_get_options_values(self):
+        "The string value of each option can be obtained using properties"
+
+        self.assertEqual(self.weekdays.mon, "mon")
+        self.assertEqual(self.weekdays.tue, "tue")
+        self.assertEqual(self.weekdays.wed, "wed")
+
+    def test_get_options_int_values(self):
+        "The int value of each option can be obtained."
+
+        self.assertEqual(self.weekdays.get_int("mon"), 1)
+        self.assertEqual(self.weekdays.get_int("tue"), 2)
+        self.assertEqual(self.weekdays.get_int("wed"), 4)
+
+    def test_get_value(self):
+        """The value of an option can be retreived. They all
+        start as False"""
+
+        happy_days = BitMask(options=self.weekdays)
+        self.assertFalse(happy_days.has(self.weekdays.mon))
+
+    def test_set_value(self):
+        "The value of an option can be setted"
+
+        happy_days = BitMask(options=self.weekdays)
+        happy_days.add(self.weekdays.mon)
+        self.assertTrue(happy_days.has(self.weekdays.mon))
+
+    def test_iterate_options(self):
+        "One can iterate over the available options"
+
+        options_available = []
+        for option in self.weekdays:
+            options_available.append(option)
+
+        self.assertEqual(options_available, self.options)
+
+    def test_query_option(self):
+        """If a MaskOptions object was provided, the mask
+        can be queried directly"""
+
+        happy_days = BitMask(options=self.weekdays)
+        happy_days.add(self.weekdays.mon)
+        self.assertTrue(happy_days.has(self.options[0])) # mon
+
+    def test_set(self):
+        "A value can be setted as enabled"
+
+        happy_days = BitMask(options=self.weekdays)
+        happy_days.set(self.weekdays.mon, True)
+        self.assertTrue(happy_days.has(self.weekdays.mon))
+
+    def test_set_false(self):
+        "A value can be setted as disabled"
+
+        happy_days = BitMask(options=self.weekdays)
+        happy_days.set(self.weekdays.mon, True)
+        self.assertTrue(happy_days.has(self.weekdays.mon))
+
+        happy_days.set(self.weekdays.mon, False)
+        self.assertFalse(happy_days.has(self.weekdays.mon))
+
+        happy_days.set(self.weekdays.mon, False)
+        self.assertFalse(happy_days.has(self.weekdays.mon))
+
+if __name__ == "__main__":
+    unittest.main()
